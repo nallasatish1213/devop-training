3f9dabaa8ceb1691474f8a337e692e919c18391d
nallasatish1213 <nalla.satish1213@gmail.com>
2024-09-09 23:13:09 +0530

    add basic scripts

---
+++ com/scripts/AddingStaticVar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1="Ramesh"
+PERSON2="Suresh"
+
+echo "First statement of shell scripts"
+echo "$PERSON1 : Hi Good morning $PERSON2"
+echo "$PERSON2 : Hello Very Good morning $PERSON1 and How are you..?"
+echo "$PERSON1 : AM good and thanks for asking $PERSON2"
+echo "End statement of shell scripts"
---
+++ com/scripts/BasicScript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "First statement of shell scripts"
+echo "Ramesh : Hi Good morning Suresh"
+echo "Suresh : Hello Very Good morning Ramesh and How are you..?"
+echo "Ramesh : AM good and thanks for asking"
+echo "End statement of shell scripts"
